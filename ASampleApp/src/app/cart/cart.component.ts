353466d6062af835ce3470348136fcef8b930779
Blackbell <shy817@naver.com>
2020-09-06 15:14:09 +0900

    ASampleApp - 폼으로입력받기

--- ASampleApp/src/app/cart/cart.component.ts
+++ ASampleApp/src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,12 +9,27 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm ;
+
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
 
   ngOnInit(): void {
     this.items = this.cartService.getItems();
   }
 
+  onSubmit(customerData) {
+    // 주문 로직은 여기에 구현합니다.
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
 }
